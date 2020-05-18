68a62e5ce324ec5d00c69cde61e7227a0c378e45
Dor Shinar <dors718@gmail.com>
2020-05-18 12:43:04 +0300

    add docs to args

--- src/args.ts
+++ src/args.ts
@@ -2,10 +2,19 @@ import { FlexArgs } from "./FlexArgs.ts";
 
 const { args } = Deno;
 
+/**
+ * If no argument is specified, throw an error
+ */
 if (args.length === 0) {
   throw new Error("No command specified!");
 }
 
+/**
+ * Any argument passed after "--" will be passed on to the executed command.
+ * For example:
+ * $ flex start -- Hello
+ * Will run the "start" script, and pass "Hello" as the first argument.
+ */
 let commandArgs: string[] | undefined;
 if (args.length > 0 && args[1] === "--") {
   commandArgs = args.slice(2);
