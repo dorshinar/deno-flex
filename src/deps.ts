6e46a667249f9104a6d789517ed417ecbcf66e97
Dor Shinar <dors718@gmail.com>
2020-05-19 12:17:10 +0300

    use fixed version deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { parse as parseYaml } from "https://deno.land/std/encoding/yaml.ts";
-export { parse as parseToml } from "https://deno.land/std/encoding/toml.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.51.0/encoding/yaml.ts";
+export { parse as parseToml } from "https://deno.land/std@0.51.0/encoding/toml.ts";
