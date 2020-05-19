28491bc9420f3d6ab844903bd66b0f7dbd64167d
Dor Shinar <dors718@gmail.com>
2020-05-19 12:30:18 +0300

    simplify file reading

--- src/readDecodedFile.ts
+++ src/readDecodedFile.ts
@@ -4,7 +4,5 @@
  * @param filename the absolute path to the file.
  */
 export async function readDecodedFile(filename: string) {
-  const decoder = new TextDecoder();
-  const rawFile = await Deno.readFile(filename);
-  return decoder.decode(rawFile);
+  return Deno.readTextFile(filename);
 }
