7c27edcd8c076176f1d0e12fa2295477b513e111
Dor Shinar <dors718@gmail.com>
2020-05-28 16:11:20 +0300

    skip js tests on windows

--- tests/e2e_test.ts
+++ tests/e2e_test.ts
@@ -1,9 +1,5 @@
+import { SupportedFile } from "./../src/types.ts";
 import { assertEquals, assertThrowsAsync, path } from "./e2e_deps.ts";
-
-console.log(
-  "Starting to run tests...",
-  JSON.stringify({ cwd: Deno.cwd() }, null, 2)
-);
 
 const flexCmd = "flex";
 
@@ -12,9 +8,6 @@ function getCWD(file: string) {
 }
 
 async function runFlex(cmd: string[], cwd: string) {
-  for await (const dir of Deno.readDir(cwd)) {
-    console.log(JSON.stringify(dir, null, 2));
-  }
   const process = Deno.run({
     cmd: [flexCmd, ...cmd],
     cwd,
@@ -44,44 +37,60 @@ async function getFlexOutput(cmd: string[], cwd: string) {
   return decodeOutput(rawOutput);
 }
 
-const fileExtensions = ["js", "json", "yaml", "yml", "toml"];
+const fileExtensions: SupportedFile[] = [
+  ".js",
+  ".json",
+  ".yaml",
+  ".yml",
+  ".toml",
+];
 const scripts = ["start", "prod"];
 
-scripts.map((script) =>
-  fileExtensions.map((ext) => {
-    Deno.test(`runs ${script} with no arguments from ${ext} file`, async () => {
-      const output = await getFlexOutput([script], getCWD(ext));
-      assertEquals(output, `${script} output`);
+fileExtensions.map((ext) => {
+  scripts.map((script) => {
+    Deno.test({
+      name: `runs ${script} with no arguments from ${ext} file`,
+      ignore: ext === ".js" && Deno.build.os == "windows",
+      async fn() {
+        const output = await getFlexOutput([script], getCWD(ext));
+        assertEquals(output, `${script} output`);
+      },
     });
 
-    Deno.test(`runs ${script} with 1 argument from ${ext} file`, async () => {
-      const args = ["--", "hello"];
-      const output = await getFlexOutput([script, ...args], getCWD(ext));
-      assertEquals(output, `${script} output hello`);
+    Deno.test({
+      name: `runs ${script} with 1 argument from ${ext} file`,
+      ignore: ext === ".js" && Deno.build.os == "windows",
+      async fn() {
+        const args = ["--", "hello"];
+        const output = await getFlexOutput([script, ...args], getCWD(ext));
+        assertEquals(output, `${script} output hello`);
+      },
     });
 
-    Deno.test(
-      `runs ${script} with arguments not preceded by '--' from ${ext} file`,
-      async () => {
+    Deno.test({
+      name: `runs ${script} with arguments not preceded by '--' from ${ext} file`,
+      ignore: ext === ".js" && Deno.build.os == "windows",
+      async fn() {
         const args = ["hello"];
         const output = await getFlexOutput([script, ...args], getCWD(ext));
         assertEquals(output, `${script} output`);
-      }
-    );
+      },
+    });
 
-    Deno.test(
-      `runs ${script} with multiple arguments from ${ext} file`,
-      async () => {
+    Deno.test({
+      name: `runs ${script} with multiple arguments from ${ext} file`,
+      ignore: ext === ".js" && Deno.build.os == "windows",
+      async fn() {
         const args = ["hello", "world"];
         const output = await getFlexOutput(
           [script, "--", ...args],
           getCWD(ext)
         );
         assertEquals(output, `${script} output ${args.join(" ")}`);
-      }
-    );
-  })
-);
+      },
+    });
+  });
+});
 
 fileExtensions.map((ext) => {
   Deno.test(`throws when running unknown script from ${ext} file`, async () => {
